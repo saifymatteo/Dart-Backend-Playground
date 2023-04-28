8b1a0d22aaa1c703174cb4100d54485d4956054a
saifymatteo <muhdsaifulmashuri@gmail.com>
2023-04-28 22:00:17 +0800

    rework deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,17 +1,8 @@
-# Build the production
-dart_frog build
-
-# Remove ignore files
-Remove-Item build\.gitignore
-Remove-Item build\.dockerignore
-
-# Copy custom Dockerfile to build
-Copy-Item Dockerfile build\
+# Rename .gitignore
+Rename-Item -Path '.gitignore' -NewName '.gitignore.backup'
 
 # Deploy Google Cloud Run
-#gcloud run deploy dart-frog --source build --project dart-frog-playground --region asia-southeast1 --allow-unauthenticated
+gcloud run deploy dart-frog --source . --project dart-frog-playground --region asia-southeast1 --allow-unauthenticated
 
-# Deploy AWS ECR
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 175192474076.dkr.ecr.ap-southeast-1.amazonaws.com
-docker build build --tag 175192474076.dkr.ecr.ap-southeast-1.amazonaws.com/dart_frog_playground:latest
-docker push 175192474076.dkr.ecr.ap-southeast-1.amazonaws.com/dart_frog_playground:latest
+# Rename back after done
+Rename-Item -Path '.gitignore.backup' -NewName '.gitignore'
